dd76e6f5952ab5930199f2e780733bfb0cfebc86
Antavion <manishsamal832@gmail.com>
2026-05-05 17:56:14 +0530

    Update replay button to remove style parameter
    Removed style parameter from replay button.

--- Elevenyts/helpers/_inline.py
+++ Elevenyts/helpers/_inline.py
@@ -34,7 +34,7 @@ class Inline:
                 [
                     self.ikb(text="▷", callback_data=f"controls resume {chat_id}"),
                     self.ikb(text="II", callback_data=f"controls pause {chat_id}"), # style=enums.ButtonStyle.PRIMARY),
-                    self.ikb(text="↻", callback_data=f"controls replay {chat_id}",style=enums.ButtonStyle.SUCCESS),
+                    self.ikb(text="↻", callback_data=f"controls replay {chat_id}"), #,style=enums.ButtonStyle.SUCCESS),
                     self.ikb(text="‣‣I", callback_data=f"controls skip {chat_id}"), # style=enums.ButtonStyle.PRIMARY),
                     self.ikb(text="▢", callback_data=f"controls stop {chat_id}"),
                 ]
