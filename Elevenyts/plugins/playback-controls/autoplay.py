9f7fd4982237319da37e16df67a5c74f680091e4
Antavion <manishsamal832@gmail.com>
2026-05-07 22:10:48 +0530

    autoplay

--- Elevenyts/plugins/playback-controls/autoplay.py
+++ Elevenyts/plugins/playback-controls/autoplay.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
-from Elevenyts import app
-from Elevenyts.storage import AUTO_PLAY
+from Elevenyts import app, AUTO_PLAY, call, yt
+from Elevenyts.helpers import Track
 
 
 @app.on_message(filters.command("autoplay") & filters.group)
@@ -14,17 +14,42 @@ async def autoplay(_, message):
 
     query = " ".join(message.command[1:])
 
-    # Turn Off
+    # OFF
     if query.lower() == "off":
         AUTO_PLAY.pop(chat_id, None)
+        return await message.reply("✅ Autoplay Disabled")
 
-        return await message.reply(
-            "✅ Autoplay Disabled"
-        )
-
-    # Save keyword
+    # SAVE QUERY
     AUTO_PLAY[chat_id] = query
 
     await message.reply(
         f"✅ Autoplay Enabled\n🎵 Keyword: {query}"
     )
+
+    # SEARCH SONG
+    try:
+        track = await yt.search(query, 0)
+
+        if not track:
+            return await message.reply("❌ Song not found")
+
+        track.is_autoplay = True
+
+        # DOWNLOAD
+        track.file_path = await yt.download(
+            track.id,
+            is_live=track.is_live
+        )
+
+        if not track.file_path:
+            return await message.reply("❌ Download failed")
+
+        # PLAY DIRECTLY
+        await call.play_media(
+            chat_id=chat_id,
+            message=None,
+            media=track
+        )
+
+    except Exception as e:
+        await message.reply(f"❌ Error:\n{e}")
