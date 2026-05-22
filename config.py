96f47c4544aa411f78fa7c17629208526b879f00
Antavion <manishsamal832@gmail.com>
2026-05-22 19:07:29 +0530

    Update START_IMGS list with new image URLs

--- config.py
+++ config.py
@@ -74,13 +74,15 @@ class Config:
 
         self.START_IMGS: List[str] = [
             "https://drive.google.com/uc?id=165bJ_UImzvD6Xn5_ndrjpL6OdxWS5HYS",
-            "https://i.pinimg.com/550x/fc/48/c6/fc48c6e50f85375fdaadb987b9aaca09.jpg",
-            "https://img.freepik.com/premium-photo/anime-girl-listening-music-while-sitting-table-with-drink-generative-ai_733139-40138.jpg",
-            "https://motionbgs.com/media/5785/goth-anime-girl.jpg",
-            "https://wallpaperbat.com/img/888697.jpg",
-            "https://image.cdn2.seaart.ai/2023-07-19/46636105711685/3d49c1baa56ecf4223342600635dd79d5d7764cd_high.webp",
-            "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/83c3ac45-0de3-4f59-8dfe-a2379b3a3400/djp0q30-25e8b9ec-6fe2-4246-adbd-b51b6aafb88a.png/v1/fill/w_1182,h_676,q_70,strp/_second_charge__by_evlsound_djp0q30-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzMyIiwicGF0aCI6IlwvZlwvODNjM2FjNDUtMGRlMy00ZjU5LThkZmUtYTIzNzliM2EzNDAwXC9kanAwcTMwLTI1ZThiOWVjLTZmZTItNDI0Ni1hZGJkLWI1MWI2YWFmYjg4YS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.TxMk8t3sbhfVyHHw0mSVQa3gM44_WAerlfgclzx8ZsA",
-            "https://moewalls.com/wp-content/uploads/2024/09/anime-girl-watching-sunset-pixel-thumb.jpg",
+            "https://drive.google.com/uc?id=1y4KnINZ7MMM6DG2tMrcysI_pTGCdKhsc",
+            "https://drive.google.com/uc?id=1RMcGcEL-ta_3L0CloFJeUbarT7GsU-B4",
+            "https://drive.google.com/uc?id=1ADYcCBdN_qazu2J9CtZjgkw0g5CeIF3R",
+            "https://drive.google.com/uc?id=16xIZgMc3dPWBie48Ui-RDm35fyhjLTMG",
+            "https://drive.google.com/uc?id=1j8p2bp0fldppSMTRMeMPGip2m-i3z8S4",
+            "https://drive.google.com/uc?id=1HAC5Ng_JCIZxRH3NtkocnrLOZtbYAD1b",
+            "https://drive.google.com/uc?id=1oOBPDjHAGGmxPsJSYf9HE82z_ZyKR2Aw",
+            "https://drive.google.com/uc?id=1XzJUUHhLnWwi3x8vmiunbhWZCm56w-x4",
+            
         ]
 
         self.RADIO_IMGS: List[str] = [
